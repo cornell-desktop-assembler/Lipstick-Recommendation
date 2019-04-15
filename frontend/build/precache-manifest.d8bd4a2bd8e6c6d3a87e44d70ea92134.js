882e7cffab024f69fc8a4209ce212e1047c0b576
jialuli-luka <790359064@qq.com>
2019-04-15 19:31:28 -0400

    build

---
+++ frontend/build/precache-manifest.d8bd4a2bd8e6c6d3a87e44d70ea92134.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f666c906a6b60999480",
+    "url": "/static/css/main.95254a02.chunk.css"
+  },
+  {
+    "revision": "3f666c906a6b60999480",
+    "url": "/static/js/main.f8b7d71a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7330ab38eeab3fca714e",
+    "url": "/static/js/2.bf094b3f.chunk.js"
+  },
+  {
+    "revision": "28f4ada6b41c4afdd1e15268d695a68c",
+    "url": "/static/media/title.28f4ada6.png"
+  },
+  {
+    "revision": "b3deff1e37c4626d8fe2d95154865fd7",
+    "url": "/static/media/background.b3deff1e.png"
+  },
+  {
+    "revision": "82423eae7201d04c6bededfc9bff1887",
+    "url": "/index.html"
+  }
+];
