8b6701968ce9d34e29a10ebfbd676bb9dab3e94b
Charlie <charliewang95@gmail.com>
2019-04-15 21:02:11 -0400

    add

---
+++ frontend/build/precache-manifest.ebb093f94f09f7fc9be7b72a89ff101e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "28f4ada6b41c4afdd1e15268d695a68c",
+    "url": "/static/media/title.28f4ada6.png"
+  },
+  {
+    "revision": "a5f0ac568d6697a5ac2742b80acc949f",
+    "url": "/static/media/background.a5f0ac56.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c897f998722211599d2a",
+    "url": "/static/js/main.c1d77d02.chunk.js"
+  },
+  {
+    "revision": "a5eddeed9442badf381f",
+    "url": "/static/js/2.98f6c5c1.chunk.js"
+  },
+  {
+    "revision": "c897f998722211599d2a",
+    "url": "/static/css/main.f06901f6.chunk.css"
+  },
+  {
+    "revision": "f5dd523d0a103a0be1771ab8687097dc",
+    "url": "/index.html"
+  }
+];
