2e08a7b19e2809c41546d7597797e91774868932
Zhen Li <zl694@cornell.edu>
2019-04-18 12:57:54 -0400

    desc: url and price

--- desc_and_img/desc_and_img.py
+++ desc_and_img/desc_and_img.py
@@ -6,6 +6,8 @@ import json
 TEMPLATE_LIPSTICK = r"https://www.sephora.com/productimages/sku/s{sku}-main-Lhero.jpg"
 TEMPLATE_TRY_ON   = r"https://www.sephora.com/productimages/sku/s{sku}-av-0{x}-Lhero.jpg"
 
+TEMPLATE_URL = r"https://www.sephora.com/product/{pid}?skuId={sku}"
+
 
 with open("sku2desc.json", encoding="utf-8") as fin:
     sku2desc = json.load(fin)
@@ -19,4 +21,6 @@ def get_img_url_by_sku(sku):
 
 
 def get_desc_by_sku(sku):
-    return sku2desc.get(sku, None)
+    desc = sku2desc.get(sku, None)
+    desc["url"] = TEMPLATE_URL.format(sku=sku, pid=desc["pid"].upper())
+    return desc
