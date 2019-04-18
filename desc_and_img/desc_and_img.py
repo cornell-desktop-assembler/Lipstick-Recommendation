86da2aa36678560f120d286e74040fe3211176b8
Zhen Li <zl694@cornell.edu>
2019-04-18 15:50:12 -0400

    skeleton of search.py, not tested yet

--- desc_and_img/desc_and_img.py
+++ desc_and_img/desc_and_img.py
@@ -13,14 +13,31 @@ with open("sku2desc.json", encoding="utf-8") as fin:
     sku2desc = json.load(fin)
 
 def get_img_url_by_sku(sku):
-    r = requests.get(TEMPLATE_LIPSTICK.format(sku=sku))
-    if r.headers.get("content-type").startswith("image"):
-        return r
-    else:
-        return None
+#     r = requests.get(TEMPLATE_LIPSTICK.format(sku=sku))
+#     if r.headers.get("content-type").startswith("image"):
+#         return r
+#     else:
+#         return None
+    return TEMPLATE_LIPSTICK.format(sku=sku)
 
 
 def get_desc_by_sku(sku):
     desc = sku2desc.get(sku, None)
     desc["url"] = TEMPLATE_URL.format(sku=sku, pid=desc["pid"].upper())
     return desc
+
+
+'''
+
+{
+  "2144616": {
+    "pid": "P09784333", 
+    "brand": "FENTY BEAUTY by Rihanna",
+    "name": "Snow Daze & Snow Nights Frosted Metal Lipstick 3-pc Set", 
+    "code": "Snow Nights No Chill, Frost Sauce, Fog Snog - Selected", 
+    "price": "$36.00"
+  }
+
+}
+
+'''
--- search.py
+++ search.py
@@ -2,7 +2,9 @@ from comment import sim_text_matcher
 from color import sim_color_matcher
 from desc_and_img import desc_and_img
 
-def search(query):
+import numpy as np
+
+def search(query, color_k=100, keywords_k=100, filter_k=100):
     keywords = query["keywords"]
     brands = query["brands"]
     skinTone = query["skinTone"]
@@ -10,3 +12,30 @@ def search(query):
     hairColor = query["hairColor"]
     eyeColor = query["eyeColor"]
     r, g, b = int(query["r"]), int(query["g"]), int(query["b"])
+    rgb = np.array([r,g,b])
+    
+    # color
+    color_result = sim_color_matcher.knn_sku(rgb, k=color_k)
+    # sku, dist = color_result[i][0], color_result[i][1]
+    
+    # TODO: keywords
+    
+    # TODO: filter
+    
+    # assume there will be a list of skus
+    skus = []
+    
+    result = []
+    
+    for i, sku in enumerate(skus):
+        d = {}
+        d["rank"] = i
+        d["sku"] = sku
+        d.update(desc_and_img.get_desc_by_sku(sku))
+        # pid, brand, name, code, price, url
+        d["img_url"] = desc_and_img.get_img_url_by_sku(sku)
+        d["keywords"] = [] # TODO
+        result.add(d)
+        
+    return result
+        
