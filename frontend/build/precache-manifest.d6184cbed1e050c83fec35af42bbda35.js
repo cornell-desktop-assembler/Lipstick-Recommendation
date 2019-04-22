3826c92d429f60a6b92cddab8b5cb61ebe256072
Charlie <charliewang95@gmail.com>
2019-04-22 16:12:40 -0400

    real build

---
+++ frontend/build/precache-manifest.d6184cbed1e050c83fec35af42bbda35.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "28f4ada6b41c4afdd1e15268d695a68c",
+    "url": "/static/media/title.28f4ada6.png"
+  },
+  {
+    "revision": "2142fb4a8e0883e12df4440c01e8f103",
+    "url": "/static/media/return4.2142fb4a.png"
+  },
+  {
+    "revision": "cc848ebd578372e4d17e0b1942277cc5",
+    "url": "/static/media/crown.cc848ebd.png"
+  },
+  {
+    "revision": "dd2624fb2b221dbd4f757a6f6db29875",
+    "url": "/static/media/background-4.dd2624fb.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4523f394e7c94153964b",
+    "url": "/static/js/main.71955bdf.chunk.js"
+  },
+  {
+    "revision": "d2cfad7ac1115660ce25",
+    "url": "/static/js/2.6b4606fc.chunk.js"
+  },
+  {
+    "revision": "4523f394e7c94153964b",
+    "url": "/static/css/main.fe81155d.chunk.css"
+  },
+  {
+    "revision": "d2cfad7ac1115660ce25",
+    "url": "/static/css/2.485bb7ac.chunk.css"
+  },
+  {
+    "revision": "6d63fb99926e841a8be89942921d10b6",
+    "url": "/index.html"
+  }
+];
